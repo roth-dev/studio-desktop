4ebe0be7a08fa676f84e7600f615845b4b5e2c86
@roth-dev <roth.dev.ops@gmail.com>
2025-01-15 21:43:46 +0700

    search connection

--- src/database/index.tsx
+++ src/database/index.tsx
@@ -2,27 +2,53 @@ import { Toolbar } from "@/components/toolbar";
 import { AnimatedRouter } from "@/components/animated-router";
 import { ConnectionCreateUpdateRoute } from "./editor-route";
 import { ConnectionStoreManager } from "@/lib/conn-manager-store";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import ImportConnectionStringRoute from "./import-connection-string";
 import useNavigateToRoute from "@/hooks/useNavigateToRoute";
 import AddConnectionDropdown from "./add-connection-dropdown";
 import ConnectionList from "@/components/database/connection-list";
+import { Input } from "@/components/ui/input";
 
 function ConnectionListRoute() {
   useNavigateToRoute();
-
+  const [search, setSearch] = useState("");
   const [connectionList, setConnectionList] = useState(() => {
     return ConnectionStoreManager.list();
   });
+
+  const connections = useMemo(() => {
+    if (search) {
+      return connectionList.filter((conn) =>
+        conn.name.toLowerCase().includes(search.toLowerCase()),
+      );
+    }
+    return connectionList;
+  }, [connectionList, search]);
+
   return (
     <div className="flex h-full w-full flex-col">
       <Toolbar>
         <AddConnectionDropdown />
+        <Input
+          value={search}
+          placeholder="Search..."
+          className="w-1/3"
+          onChange={(e) => {
+            e.preventDefault();
+            setSearch(e.currentTarget.value);
+          }}
+        />
       </Toolbar>
-      <ConnectionList
-        data={connectionList}
-        setConnectionList={setConnectionList}
-      />
+      {!!search && connections.length === 0 ? (
+        <div className="flex flex-1 justify-center p-3 text-sm text-neutral-600">
+          Search not found.
+        </div>
+      ) : (
+        <ConnectionList
+          data={connections}
+          setConnectionList={setConnectionList}
+        />
+      )}
     </div>
   );
 }
